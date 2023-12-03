6723bfdfda7b9d845914f485d09c2cb760804a2f
RubenY <rubenytsma@icloud.com>
2023-12-03 21:08:56 +0100

    Dingen die ik eerst perongeluk niet pushte nu wel pushen :))

--- Bronbestanden/PID/PID_test2/PID_test2.ino
+++ Bronbestanden/PID/PID_test2/PID_test2.ino
@@ -1,7 +1,13 @@
+#include <Servo.h>
 #include <Wire.h>
 #include <PID_v1.h>
 
-const int MPU_addr=0x69;
+Servo servo_x1;
+Servo servo_x2;
+Servo servo_z1;
+Servo servo_z2;
+
+const int MPU_addr=0x68;
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
  
 int minVal=265;
@@ -20,9 +26,9 @@ double Cal_z;
 double correct_x;
 double correct_z;
 
-const double Kp = 2;
-const double Ki = 5;
-const double Kd = 1;
+const double Kp = 0.2;
+const double Ki = 0.5;
+const double Kd = 0.1;
 
 const int servo_0 = 90;
 
@@ -30,6 +36,22 @@ PID myPIDx(&x, &correct_x, &Cal_x, Kp, Ki, Kd, DIRECT);
 PID myPIDz(&z, &correct_z, &Cal_z, Kp, Ki, Kd, DIRECT);
 
 void setup() {
+  myPIDx.SetOutputLimits(-45, 45);
+  myPIDz.SetOutputLimits(-45, 45);
+  myPIDx.SetMode(AUTOMATIC);
+  myPIDz.SetMode(AUTOMATIC);
+
+  // attaches a servo to a pin
+  servo_x1.attach(3);
+  servo_x2.attach(5);
+  servo_z1.attach(9);
+  servo_z2.attach(10);
+
+  servo_x1.write(servo_0);
+  servo_x2.write(servo_0);
+  servo_z1.write(servo_0);
+  servo_z2.write(servo_0);
+
   // Starts the communication with the gyro
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
@@ -76,6 +98,11 @@ void loop() {
   Serial.println(Cal_z);
   Serial.print("Output: ");
   Serial.println(correct_z);
+
+  servo_x1.write(servo_0 - correct_x);
+  servo_x2.write(servo_0 + correct_x);
+  servo_z1.write(servo_0 - correct_z);
+  servo_z2.write(servo_0 + correct_z);
 
   delay(5);
 }
