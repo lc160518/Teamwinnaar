6182adbd03a7f6781ded1455588632d162de6a84
Julian <113358009+lc160518@users.noreply.github.com>
2024-03-10 17:30:18 +0100

    added slider

--- Bronbestanden/De laatste weken/EDF-Sketch/EDF-Sketch.ino
+++ Bronbestanden/De laatste weken/EDF-Sketch/EDF-Sketch.ino
@@ -17,33 +17,39 @@ void handleRoot() {
   page += "<head><title>ESP32 Slider Control</title></head>\n";
   page += "<body>\n";
   page += "<h1>ESP32 Slider Control</h1>\n";
-  page += "<input type=\"range\" min=\"0\" max=\"100\" value=\"" + String(sliderValue) + "\" class=\"slider\" id=\"myRange\" name=\"value\" style=\"width: 80%;\">\n";  // Adjusted size here
+  page += "<input type=\"range\" min=\"0\" max=\"100\" value=\"" + String(sliderValue) + "\" class=\"slider\" id=\"myRange\" name=\"value\" style=\"width: 80%;\">\n";
   page += "<p>Value: <span id=\"demo\">" + String(sliderValue) + "</span></p>\n";
-  page += "<button onclick=\"setToZero()\" style=\"font-size: 30px; padding: 20px;\">ABORT</button>\n";  // Larger button with increased font size and padding
+  page += "<input type=\"number\" id=\"manualInput\" value=\"" + String(sliderValue) + "\" min=\"0\" max=\"100\" style=\"width: 80%;\">\n";
+  page += "<button onclick=\"setToZero()\" style=\"font-size: 30px; padding: 20px;\">ABORT</button>\n";
+  page += "<button onclick=\"updateSliderFromInput()\" style=\"font-size: 30px; padding: 20px;\">Update Slider</button>\n";
   page += "<script>\n";
   page += "var slider = document.getElementById(\"myRange\");\n";
   page += "var output = document.getElementById(\"demo\");\n";
-  page += "var requestInProgress = false;\n";  // Flag to track if a request is in progress
+  page += "var manualInput = document.getElementById(\"manualInput\");\n";
   page += "output.innerHTML = slider.value;\n";
   page += "slider.oninput = function() {\n";
   page += "  output.innerHTML = this.value;\n";
-  page += "  if (!requestInProgress) {\n";  // Only send request if no request is in progress
-  page += "    updateSlider(this.value);\n";
+  page += "  updateSlider(this.value);\n"; // Update the slider value and send the update
+  page += "}\n";
+  page += "function updateSliderFromInput() {\n";
+  page += "  var inputValue = parseInt(manualInput.value);\n";
+  page += "  if (!isNaN(inputValue) && inputValue >= 0 && inputValue <= 100) {\n";
+  page += "    slider.value = inputValue;\n";
+  page += "    output.innerHTML = inputValue;\n"; // Update the displayed value next to the slider
+  page += "    updateSlider(inputValue);\n";
   page += "  }\n";
   page += "}\n";
   page += "function updateSlider(val) {\n";
-  page += "  requestInProgress = true;\n";  // Set flag to indicate a request is in progress
   page += "  var xhttp = new XMLHttpRequest();\n";
   page += "  xhttp.onreadystatechange = function() {\n";
   page += "    if (this.readyState == 4 && this.status == 200) {\n";
   page += "      console.log('Value updated');\n";
-  page += "      requestInProgress = false;\n";  // Reset flag when request is complete
   page += "    }\n";
   page += "  };\n";
   page += "  xhttp.open(\"GET\", \"/update?value=\" + val, true);\n";
   page += "  xhttp.send();\n";
   page += "}\n";
-  page += "function setToZero() {\n";  // Function to set value to 0
+  page += "function setToZero() {\n";
   page += "  slider.value = 0;\n";
   page += "  output.innerHTML = 0;\n";
   page += "  updateSlider(0);\n";
@@ -54,12 +60,14 @@ void handleRoot() {
   server.send(200, "text/html", page);
 }
 
+
+
 void handleUpdate() {
   if (server.hasArg("value")) {
     String valueStr = server.arg("value");
     sliderValue = valueStr.toInt();
-    escValue = map(sliderValue, 0, 100, 1000, 2600);
-    constrain(escValue, 1000, 2600);
+    escValue = map(sliderValue, 0, 100, 1000, 3000);
+    escValue = constrain(escValue, 1000, 3000);
 
   }
   server.send(200, "text/plain", "OK");
